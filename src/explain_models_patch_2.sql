0cd73c5ed4e32195a644ab8cb40fc293a9db3789
Prohor Sevastianov <sevastyanov.p@oneway.su>
2021-02-03 23:56:28 +0300

    lesson3

---
+++ src/explain_models_patch_2.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_buyPrice ON products(buyPrice);
+CREATE INDEX idx_buyPrice ON productvariants(buyPrice);
+CREATE INDEX idx_productCode ON productvariants(productCode);
+CREATE INDEX idx_productLine ON products(productLine);
